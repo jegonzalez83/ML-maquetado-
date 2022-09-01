459690a3174d29015a94b8defb55bfc4383d6495
Jhon González <enriquemime@gmail.com>
2022-09-01 12:43:00 -0300

    creacion repo github

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3030;
+const path = require ('path')
+app.use(express.static('public'))
+
+
+app.get( '/', function(req,res){res.sendFile(path.join(__dirname, '/views/home.html'))})
+
+
+app.listen(port,()=> console.log(`SERVIDOR LEVANTADO EN EL PUERTO ${port}`))
